58db399fb76d74d62a4da3b5c85294873d6f4c01
BrenoMOliveira <brenomoliveira03@gmail.com>
2025-01-13 17:09:43 -0300

    style: customizing theme and adjusting photo

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -5,8 +5,9 @@ import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 const Hero = () => {
   const StyleHero = styled("div")(() => ({
-    backgroundColor: "black",
+    background: "linear-gradient(140deg, #000000, rgba(44,0,89,0.9), #000000)",
     height: "100vh",
+    margin: "0px"
   }));
 
   const StyleImg = styled("img")(() => ({
@@ -17,12 +18,12 @@ const Hero = () => {
   return (
     <StyleHero>
       <Container maxWidth={"lg"}>
-        <Grid container spacing={2}>
+        <Grid container spacing={2} margin={"0px"}>
           <Grid xs={12} md={4}>
             <StyleImg src={Profile} alt="Profile" />
           </Grid>
           <Grid xs={12} md={8}>
-            <Typography color="primary" variant="h1" textAlign={"center"}>Breno Martins Oliveira</Typography>
+            <Typography color="primary" variant="h1" textAlign={"center"}>Breno Oliveira</Typography>
             <Typography color="primary" variant="h2" textAlign={"center"}>Sou Engenheiro de Software</Typography>
             <Grid container display={"flex"} justifyContent={"center"}>
               <Grid item xs={12} md={3} display={"flex"} justifyContent={"center"}>
--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,10 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
-import { green, purple } from '@mui/material/colors';
+import { green} from '@mui/material/colors';
 
 let theme = createTheme({
   palette: {
     primary: {
-      main: purple[500],
+      main: "#00ff85",
     },
     secondary: {
       main: green[500],
